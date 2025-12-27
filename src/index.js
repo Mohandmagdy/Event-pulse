0e578e86092c8f50bd0a9df430c0c3b79d7d3570
Mohandmagdy <mohandmagdii45@gmail.com>
2025-12-27 16:11:21 +0200

    connected with redis for email verification (otp)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { createClient } = require('redis');
 const authRoutes = require('./routes/authRoutes');
 const { authorizer } = require('./middlewares/authVaildation');
 const cookieParser = require('cookie-parser');
@@ -14,16 +15,25 @@ const mongo_port = 27017;
 const mongo_username = process.env.MONGO_USER;
 const mongo_password = process.env.MONGO_PASSWORD;
 const mongo_dbname = 'event_pulse';
-const URI = `mongodb://${mongo_username}:${mongo_password}@${mongo_host}:${mongo_port}/${mongo_dbname}?authSource=admin`;
+const mongo_URI = `mongodb://${mongo_username}:${mongo_password}@${mongo_host}:${mongo_port}/${mongo_dbname}?authSource=admin`;
 const mongo_connect = async () => {
     try {
-        await mongoose.connect(URI);
+        await mongoose.connect(mongo_URI);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
     }
 }
 mongo_connect();
+
+//connect to redis
+const redis_host = 'redis';
+const redis_port = 6379;
+const redis_URI = `redis://${redis_host}:${redis_port}`;
+const redisClient = createClient({ url: redis_URI });
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.connect().then(() => console.log('Redis client connected'));
+console.log('Connected to Redis');
 
 //middlewares
 app.use(express.json());
